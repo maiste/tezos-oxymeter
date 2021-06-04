bac20c2a5af10f70f524e7dc3ace1db15e10ac87
Etienne MARAIS <etienne.marais@nomadic-labs.com>
2021-06-04 11:12:34 +0200

    Better data display

--- lib/smartpower.ml
+++ lib/smartpower.ml
@@ -17,15 +17,23 @@ let delete sock = sock >|= fun sock -> shutdown sock SHUTDOWN_ALL
 let float_re = Re.Str.regexp "[0-9]+[.][0-9]+"
 
 let filter raw_data =
-  let raw_data = String.split_on_char ',' raw_data |> List.map String.trim in
-  let filter_float raw_data =
-    let open Re.Str in
-    if string_match float_re raw_data 0 then
-      matched_string raw_data |> Float.of_string
-    else raise (Failure "Not_found")
-    (* FIXME: improve error *)
+  let splitted_raw_data =
+    String.split_on_char ',' raw_data |> List.map String.trim
   in
-  List.map filter_float raw_data
+  let length = List.length splitted_raw_data in
+  if length <> 4 then
+    raise
+      (Failure
+         Format.(
+           sprintf "Got list of size %d instead of 4 : %s" length raw_data))
+  else
+    let filter_float raw_data =
+      let open Re.Str in
+      if string_match float_re raw_data 0 then
+        matched_string raw_data |> Float.of_string
+      else raise (Failure ("Data can't be parse: " ^ raw_data))
+    in
+    List.map filter_float splitted_raw_data
 
 let get_data_from sock =
   let* sock = sock in
@@ -50,14 +58,12 @@ let json_of_report energy_report =
 let observe sock =
   let time = Unix.gettimeofday () in
   let* energy_data = observe_raw sock in
-  if List.length energy_data <> 4 then raise (Failure "Not_found") (* FIXME *)
-  else
-    let energy_array = Array.of_list energy_data in
-    let time = ("time", time) in
-    let volt = ("tension", energy_array.(0)) in
-    let ampere = ("ampere", energy_array.(1)) in
-    let power = ("power", energy_array.(2)) in
-    let watt_hour = ("watt_hour", energy_array.(3)) in
-    let energy_report = [ time; volt; ampere; power; watt_hour ] in
-    let energy_report_json = json_of_report energy_report in
-    Lwt.return energy_report_json
+  let energy_array = Array.of_list energy_data in
+  let time = ("time", time) in
+  let volt = ("tension", energy_array.(0)) in
+  let ampere = ("ampere", energy_array.(1)) in
+  let power = ("power", energy_array.(2)) in
+  let watt_hour = ("watt_hour", energy_array.(3)) in
+  let energy_report = [ time; volt; ampere; power; watt_hour ] in
+  let energy_report_json = json_of_report energy_report in
+  Lwt.return energy_report_json
