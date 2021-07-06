8f0d14f7fd167cddcf043037222e591c071663c5
Etienne MARAIS <etienne.marais@nomadic-labs.com>
2021-07-06 12:31:20 +0200

    Extract ppx as public lib

---
+++ ppx/ppx_oximeter.ml
@@ -0,0 +1,33 @@
+open Ppxlib
+open Tezos_oxymeter
+
+let wrap_expr loc expr name =
+  let fun_name = Ast_builder.Default.estring ~loc name in
+  let file = Ast_builder.Default.estring ~loc loc.loc_start.pos_fname in
+  [%expr
+    Metrics.TimeMetrics.insert [%e file] [%e fun_name] `Start ;
+    let save = [%e expr] in
+    Metrics.TimeMetrics.insert [%e file] [%e fun_name] `Stop ;
+    save]
+
+class oxymeter_mapper_v3 =
+  object (_self)
+    inherit Ast_traverse.map as super
+
+    method! expression expr =
+      let expr = super#expression expr in
+      let loc = expr.pexp_loc in
+      match expr.pexp_desc with
+      | Pexp_apply
+          ({ pexp_desc = Pexp_ident { txt = Lident name; _ }; _ }, _args) ->
+          if Metrics.TimeMetrics.exist loc.loc_start.pos_fname name then
+            let new_expr = wrap_expr loc expr name in
+            new_expr
+          else expr
+      | _ -> expr
+  end
+
+let () =
+  let ast_mapper = new oxymeter_mapper_v3 in
+  let impl = ast_mapper#structure in
+  Driver.register_transformation "ppx-oxymeter" ~impl
