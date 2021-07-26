4ebeb0dc149d5c2f88b33e2b8ee73440c6ad361d
Etienne MARAIS <etienne.marais@nomadic-labs.com>
2021-07-26 16:44:30 +0200

    Update example

--- examples/main.ml
+++ examples/main.ml
@@ -3,35 +3,36 @@ open Tezos_oxymeter
 
 let observe_smart () =
   let open Lwt.Infix in
-  let smartpower = Smartpower.create "127.0.0.1" 50500 in
-  let obs = Smartpower smartpower in
+  let smartpower = Observer.Smartpower.create "127.0.0.1" 50500 in
+  let obs = Observer.Smartpower smartpower in
   let nb_iter = 10 in
   let () = Format.printf "Observe smartpower@." in
   let rec aux k =
     if k < nb_iter then (
       Format.printf "Loop %d@." k ;
-      (observe obs >|= fun content -> Format.printf "%s@." (to_string content))
+      ( Observer.observe obs >|= fun content ->
+        Format.printf "%s@." (Observer.to_string content) )
       >>= fun () -> aux (k + 1))
     else Lwt.return_unit
   in
   aux 0
 
 let observe_blind () =
-  let obs = Blind in
+  let obs = Observer.Blind in
   let () = Format.printf "Observe bind@." in
-  (observe obs >|= fun json -> to_string json) >|= fun str ->
+  (Observer.observe obs >|= fun json -> Observer.to_string json) >|= fun str ->
   Format.printf "%s@." str
 
 let observe_mock () =
-  let obs = Mock in
+  let obs = Observer.Mock in
   let () = Format.printf "Observe mock@." in
-  (observe obs >|= fun json -> to_string json) >|= fun str ->
+  (Observer.observe obs >|= fun json -> Observer.to_string json) >|= fun str ->
   Format.printf "%s@." str
 
 let observe_mammut () =
-  let obs = Mammut None in
+  let obs = Observer.Mammut None in
   let () = Format.printf "Observe mammut@." in
-  (observe obs >|= fun json -> to_string json) >|= fun str ->
+  (Observer.observe obs >|= fun json -> Observer.to_string json) >|= fun str ->
   Format.printf "%s@." str
 
 let () =
