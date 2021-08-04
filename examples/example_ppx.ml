cc74409890150da1d27374b191e90f8e30360465
Etienne MARAIS <etienne.marais@nomadic-labs.com>
2021-08-04 10:36:57 +0200

    Push example

--- examples/example_ppx.ml
+++ examples/example_ppx.ml
@@ -1,1 +1,18 @@
-let () = Format.printf "I'm here!"
+let call () =
+  Unix.sleep 30 ;
+  Format.printf "I'm here!"
+
+let () =
+  Format.printf "Call no loop@." ;
+  call ()
+
+let () =
+  Format.printf "Call in loop@." ;
+  let rec loop k =
+    if k < 3 then (
+      Format.printf "Loop: %d@." k ;
+      call () ;
+      loop (k + 1))
+    else ()
+  in
+  loop 0
